46060468271e33feb2cffbff1642218c129088b9
Mark Fisher <mark.j.fisher@gmail.com>
2024-05-21 23:14:26 +0100

    More functions

--- src/main.c
+++ src/main.c
@@ -8,17 +8,28 @@ NetConfig nc;
 SSIDInfo info;
 
 int main() {
+	bool got_ssid = false;
 	uint8_t r = 0;
 	uint8_t i = 0;
 
 	message();
 
-	test_wifi_status();
-	// test_get_ssid();
+	// test_wifi_status();
+	// got_ssid = test_get_ssid();
+	// if (got_ssid) test_set_ssid(&nc);
+
 	// r = test_scan_for_networks();
 	// for (i = 0; i < r; i++) {
 	// 	test_get_scan_result(i);
 	// }
+
+	// test_fuji_mount_host_slot(2);
+	// test_fuji_mount_host_slot(7);
+
+	test_fuji_mount_host_slot(0);
+#include <ascii_charmap.h>
+	test_fuji_open_directory(0, "/c64/", "t*");
+#include <cbm_petscii_charmap.h>
 
 	return 0;
 }
@@ -49,7 +60,7 @@ uint8_t test_scan_for_networks() {
 }
 
 
-void test_get_ssid() {
+bool test_get_ssid() {
 	bool is_success = false;
 	is_success = fuji_get_ssid(&nc);
 	if (!is_success) {
@@ -59,6 +70,7 @@ void test_get_ssid() {
 		printf("  ssid: %s\n", nc.ssid);
 		printf("  pass: %s\n", nc.password);
 	}
+	return is_success;
 }
 
 void test_wifi_status() {
@@ -69,6 +81,47 @@ void test_wifi_status() {
 		printf("failed to get wifi_status\n");
 	} else {
 		printf("wifi_status: %d\n", wifi_status);
+	}
+}
+
+void test_set_ssid() {
+	// nc already set
+	bool is_success = false;
+
+	printf("Setting ssid...\n");
+	is_success = fuji_set_ssid(&nc);
+	if (is_success) {
+		printf("wrote ssid successfully\n");
+	} else {
+		printf("failed to set ssid\n");
+	}
+}
+
+void test_fuji_mount_host_slot(uint8_t hs) {
+	bool is_success = false;
+
+	printf("mounting host slot %d\n", hs);
+	is_success = fuji_mount_host_slot(hs);
+	if (is_success) {
+		printf("success\n");
+	} else {
+		printf("failed\n");
+	}
+}
+
+void test_fuji_open_directory(uint8_t hs, char *path, char *filter) {
+	bool is_success = false;
+
+	printf("opening %s on hs %d\n", path, hs);
+	if (filter != NULL) {
+		printf("filter set: %s\n", filter);
+	}
+
+	is_success = fuji_open_directory2(hs, path, filter);
+	if (is_success) {
+		printf("success\n");
+	} else {
+		printf("failed\n");
 	}
 }
 
--- src/main.h
+++ src/main.h
@@ -5,8 +5,11 @@
 
 void message();
 void test_wifi_status();
-void test_get_ssid();
+bool test_get_ssid();
 uint8_t test_scan_for_networks();
 void test_get_scan_result(uint8_t i);
+void test_set_ssid();
+void test_fuji_mount_host_slot(uint8_t hs);
+void test_fuji_open_directory(uint8_t hs, char *path, char *filter);
 
 #endif // MAIN_H
